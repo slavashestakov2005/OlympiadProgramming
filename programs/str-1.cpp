5ee9c582997d6f8777eb9893be051783c7c1b2d8
slavashestakov2005 <slavashestakov2005@gmail.com>
2024-02-26 00:53:09 +0300

    Отредактирована глава про строки

--- programs/str-1.cpp
+++ programs/str-1.cpp
@@ -1,14 +1,15 @@
-vector<int> zf(string s){
+vector<int> zf(string s) {
     int n = s.size(), l = 0, r = 0;
     vector<int> z(n);
     z[0] = 0;
-    for(int i = 1; i < n; ++i){
-        z[i] = 0;
-        if (l <= i && i <= r) z[i] = z[i - l];
-        if (z[i] + i > r) z[i] = r - i - 1;
-        if (z[i] < 0) z[i] = 0;
-        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
-        if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+    for(int i = 1; i < n; ++i) {
+        if (i > r) z[i] = 0;
+        else {
+            int j = i - l;
+            z[i] = min(z[j], r - i);
+        }
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+        if (i + z[i] > r) l = i, r = i + z[i];
     }
     return z;
 }
